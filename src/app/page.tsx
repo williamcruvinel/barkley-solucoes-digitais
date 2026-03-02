e23eb2d80d39441d5238252821b0e1f9c82144e0
william cruvinel <williamcruvinel@hotmail.com>
2026-03-02 20:31:34 -0300

    criate Hero Section Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Header from '@/components/Header/Header';
+import HeroSection from '@/components/Hero/HeroSection';
 
 export default function Home() {
   return (
     <>
       <Header />
-      
+      <HeroSection />
     </>
   );
 }
--- src/components/Hero/HeroSection.tsx
+++ src/components/Hero/HeroSection.tsx
@@ -1,39 +1,46 @@
-import Image from 'next/image'
-import styles from './HeroSection.module.css'
+import Image from 'next/image';
+import styles from './HeroSection.module.css';
 
 function HeroSection() {
   return (
     <section className={styles.hero}>
       <div className={styles.container}>
-        
         <div className={styles.content}>
-          <h1>
+          <h1 className={styles.title}>
             Impulsionamos negócios com estratégia e tecnologia
           </h1>
 
-          <p>
+          <p className={styles.description}>
             Somos a Barkley Soluções Digitais, uma agência focada em resultados.
-            Desenvolvemos soluções criativas e estratégicas para acelerar o crescimento da sua marca.
+            Desenvolvemos soluções criativas e estratégicas para acelerar o
+            crescimento da sua marca.
           </p>
 
-          <button className={styles.button}>
-            Fale Conosco
-          </button>
+          <button className={styles.button}>Fale Conosco</button>
         </div>
 
-        <div className={styles.image}>
+        <div className={styles.imageWrapper}>
           <Image
-            src="/images/hero.jpg"
+            src="/heroOne.png"
             alt="Equipe trabalhando"
-            width={500}
-            height={400}
+            fill
             priority
+            sizes="(max-width: 768px) 100vw, 500px"
+            className={styles.imageOne}
+          />
+
+            <Image
+            src="/heroTwo.png"
+            alt="Equipe trabalhando"
+            fill
+            priority
+            sizes="(max-width: 768px) 100vw, 500px"
+            className={styles.imageTwo}
           />
         </div>
-
       </div>
     </section>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
